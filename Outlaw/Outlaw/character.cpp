7b4809dc08d6f1a29d6355a901fade5266351bc1
buzzon <buzzondev@gmail.com>
2018-03-28 23:27:12 +0500

    Вызвано исключение: нарушение доступа для чтения. **this** было 0x30.

--- Outlaw/Outlaw/character.cpp
+++ Outlaw/Outlaw/character.cpp
@@ -1,1 +1,18 @@
 #include "character.h"
+
+void character::Update() {
+	glutPostRedisplay(); //Обновляем экран
+	Vector velocity = Velocity.GetNormalize(); //Нормализуем вектор скорости
+	if (velocity.Len != 0) //Если есть скорость то
+	{
+		CurrentAnimation = 1; //Включаем анимацию передвижения
+		Position.X += velocity.X * speed; //Добавляем к вектору игрока вектор его скорости
+		Position.Y += velocity.Y * speed;
+	}
+	else //Иначе
+	{
+		CurrentFrame = 0; //Текущий кадр - нулевой (стоим)
+		CurrentAnimation = 0; //Текущая анимация нулевая (стоим)
+	}
+	glutTimerFunc(200, character::StaticUpdate, 0); // Задержка 20 мс перед новым вызовом функции
+}
--- Outlaw/Outlaw/character.h
+++ Outlaw/Outlaw/character.h
@@ -55,22 +55,7 @@ public:
 		thePtr->Update();
 	}
 
-	void Update() {
-		glutPostRedisplay(); //Обновляем экран
-		Vector velocity = Velocity.GetNormalize(); //Нормализуем вектор скорости
-		if (velocity.Len != 0) //Если есть скорость то
-		{
-			CurrentAnimation = 1; //Включаем анимацию передвижения
-			Position.X += velocity.X * speed; //Добавляем к вектору игрока вектор его скорости
-			Position.Y += velocity.Y * speed;
-		}
-		else //Иначе
-		{
-			CurrentFrame = 0; //Текущий кадр - нулевой (стоим)
-			CurrentAnimation = 0; //Текущая анимация нулевая (стоим)
-		}
-		glutTimerFunc(200, character::StaticUpdate, 0); // Задержка 20 мс перед новым вызовом функции
-	}
+	void Update();
 	//void Animation(int value) //Обновляем Кадры исходя из текущей анимации
 	//{
 	//	if (player.CurrentAnimation == 1)
